aceb42440ffdaf89559c9a9b9384a2ea0f01b8a1
Frank <bmwsgfrank@gmail.com>
2022-01-31 13:20:08 +0300

    fileter registrar

--- centres/admin.py
+++ centres/admin.py
@@ -3,7 +3,7 @@ from django.db.models.base import Model
 from leaflet.admin import LeafletGeoAdmin, LeafletGeoAdminMixin
 from django.urls import reverse
 from django.contrib import admin
-from .models import Accreditation, Department, ExamSeries, Program, ProgramCategory, Region, PoliceStation, Centres, District, person
+from .models import Accreditation, Bank, Department, ExamSeries, Program, ProgramCategory, Region, PoliceStation, Centres, District, person
 from import_export.admin import ImportExportModelAdmin
 from django.utils.http import urlencode
 from django.utils.html import format_html
@@ -14,7 +14,9 @@ from django.utils.html import format_html
 class personAdmin(ImportExportModelAdmin):    
     list_display = ("fullname", "email", "notes",)
 
-
+@admin.register(Bank)
+class BankAdmin(ImportExportModelAdmin):
+    pass
 
 #     list_display = ("centreno", "centrename", "station","view_Programs_link","location","district","owner","centre_head","centre_registrar","accreditation_staus","accreditation_expiry")
 #     # list_filter = ("prg_code","prg_category" )
@@ -53,6 +55,26 @@ class LocationFilter(admin.SimpleListFilter):
         if self.value() == "No":
             return queryset.filter(location__isnull=True)
 
+class RegistrarFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Has Registrar'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'centre_registrar'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Yes', 'Has Registrar'),
+            ('No', 'No Registrar'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "Yes":
+            return queryset.exclude(centre_registrar__isnull=True)
+        if self.value() == "No":
+            return queryset.filter(centre_registrar__isnull=True)
+
 
 class AccreditationInline(admin.TabularInline):
     model = Accreditation
@@ -61,7 +83,7 @@ class AccreditationInline(admin.TabularInline):
 class CentresAdmin(LeafletGeoAdmin, ImportExportModelAdmin):
     list_display = ("centreno", "centrename","centre_head",'centre_registrar', "location", "station","owner","accreditation_staus")
     search_fields = ['centreno','centrename']
-    list_filter = [LocationFilter,'owner','accreditation_staus','accreditation_staus',"station"]
+    list_filter = [LocationFilter,RegistrarFilter,'owner','accreditation_staus','accreditation_staus',"station"]
     # list_editable = ['owner','accreditation_staus','location']
     inlines = [AccreditationInline]
 
