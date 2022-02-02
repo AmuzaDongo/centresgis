4fb97871c7ae5976c384ddd34d407b697e8feaf2
Frank <bmwsgfrank@gmail.com>
2022-02-02 14:48:25 +0300

    fix person names

--- Awards/models.py
+++ Awards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Committee(models.Model):
+
+    
+
+    class Meta:
+        verbose_name = _("committee")
+        verbose_name_plural = _("committees")
+
+    def __str__(self):
+        return self.committeename
+
+    # def get_absolute_url(self):
+    #     return reverse("committee_detail", kwargs={"pk": self.pk})
